af8b8c71b404423df611466c5075961fff0ac965
weizwz <1124725517@qq.com>
2025-05-05 20:33:23 +0800

    修改主色调

--- src/app/components/head.tsx
+++ src/app/components/head.tsx
@@ -14,12 +14,12 @@ export default function Head() {
             height={28}
             priority
           />
-          <div className="text-xl ml-2 font-bold font-mono text-indigo-700">cover
+          <div className="text-xl ml-2 font-bold font-mono text-primary">cover
             <span className="hidden md:inline"> - 封面制作器</span>
           </div>
         </div>
-        <div className="h-full flex flex-1 items-center justify-end">
-          <Button className="bg-indigo-700 hover:bg-indigo-500 rounded-3xl">
+        <div className="h-full flex items-center justify-end">
+          <Button className="rounded-3xl">
             <a href="https://github.com/weizwz/cover" target="_blank">⭐ Star on Github</a>
           </Button>
         </div>
