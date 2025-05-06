a425582180ac9d6c883789e1f7bfd70066dc3db2
weizwz <1124725517@qq.com>
2025-05-07 00:22:32 +0800

    虚拟下拉框

--- src/app/components/editorSetting.tsx
+++ src/app/components/editorSetting.tsx
@@ -8,11 +8,12 @@ import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@
 
 import { useContext, useState } from 'react'
 import { CoverContext } from './coverContext'
-import { DEFAULT_SETTING } from '../settings/default'
+import IconSelect from './iconSelect'
+// import { DEFAULT_SETTING } from '../settings/default'
 
 const EditorSetting = () => {
   const { coverSetting } = useContext(CoverContext)
-  const [setting, setSetting] = useState(DEFAULT_SETTING)
+  const [setting, setSetting] = useState(coverSetting)
 
   return (
     <div className='h-full w-full overflow-y-auto p-4'>
@@ -23,7 +24,7 @@ const EditorSetting = () => {
             <Label htmlFor='title'>标 题</Label>
             <Textarea
               id='title'
-              className='flex-1'
+              className='flex-1 focus-visible:ring-1'
               placeholder='请输入封面标题'
               defaultValue={setting.title}
               onChange={(e) => setSetting({ ...setting, title: e.target.value })}
@@ -33,11 +34,15 @@ const EditorSetting = () => {
             <Label htmlFor='author'>作 者</Label>
             <Input
               id='author'
-              className='flex-1'
+              className='flex-1 focus-visible:ring-1'
               placeholder='请输入作者'
               defaultValue={setting.author}
               onChange={(e) => setSetting({ ...setting, author: e.target.value })}
             />
+          </div>
+          <div className='flex space-x-1.5'>
+            <Label htmlFor='author'>图 标</Label>
+            <IconSelect onChange={(selectedOption) => setSetting({ ...setting, icon: selectedOption as IconOption })}/>
           </div>
           <div className='flex space-x-1.5'>
             <Label htmlFor='framework'>作者</Label>
---
+++ src/app/components/iconSelect.tsx
@@ -0,0 +1,111 @@
+'use client'
+
+import React, { useEffect, useState } from 'react'
+import Select, { OptionProps } from 'react-select'
+import type { ActionMeta, MenuListProps, MultiValue, SingleValue } from 'react-select'
+import { FixedSizeList as List } from 'react-window'
+import { DEFAULT_ICON } from '../settings/default'
+
+// 设置Iconify API的URL
+const iconifyHost = process.env.NEXT_PUBLIC_API_ICONIFY_URL
+const defaultIcon = DEFAULT_ICON
+
+// 自定义 Option 渲染组件
+const CustomOption = ({ data, isFocused, isSelected, innerRef, innerProps }: OptionProps<IconOption>) => {
+  return (
+    <div
+      ref={innerRef}
+      {...innerProps}
+      className={`p-2 ${isFocused || isSelected ? 'bg-gray-100' : ''}`}>
+      <div className='flex items-center'>
+        <span className='mr-2'>{data.label}</span>
+        <div className='ml-auto mr-2'>
+          <img loading='lazy' width={24} height={24} src={`${iconifyHost}/simple-icons/${data.value}.svg`} alt={`${data.label} icon`} />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+// 自定义 MenuList 渲染组件，使用 react-window 虚拟化
+const MenuList = ({ options, children, maxHeight, getValue }: MenuListProps<IconOption>) => {
+  const [value] = getValue()
+  const [initialOffset, setInitialOffset] = useState(0)
+
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      const foundIndex = options.findIndex((option) => !('options' in option) && option.value === value?.value)
+      setInitialOffset(foundIndex * 40) // 每项高度为 40
+    }
+  }, [options, value])
+
+  return (
+    <List height={maxHeight} width='100%' itemCount={options.length} itemSize={40} initialScrollOffset={initialOffset}>
+      {({ index, style }) => (
+        <div style={style}>
+          {Array.isArray(children) &&
+            React.isValidElement(children[index]) &&
+            React.cloneElement(children[index], {
+              key: index
+            })}
+        </div>
+      )}
+    </List>
+  )
+}
+
+const IconSelect = ({ onChange = (newValue?: SingleValue<IconOption> | MultiValue<IconOption>) => {} }) => {
+  const [selectValue, setSelectValue] = useState<SingleValue<IconOption>>(defaultIcon)
+  const [selectOptions, setSelectOptions] = useState([defaultIcon])
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        if (!iconifyHost) {
+          throw new Error('Iconify API URL is not defined')
+        }
+        const response = await fetch(iconifyHost + '/collection?prefix=simple-icons&chars=true&aliases=true')
+        const result = await response.json()
+
+        const options = result.uncategorized.map((item: any) => ({
+          value: item,
+          label: item
+        }))
+
+        setSelectOptions([defaultIcon, ...options])
+        setLoading(false)
+      } catch (error) {
+        console.error('加载图标失败:', error)
+        setLoading(false)
+      }
+    }
+
+    fetchData()
+  }, [])
+
+  const handleChange = (newValue: MultiValue<IconOption> | SingleValue<IconOption>, actionMeta: ActionMeta<IconOption>) => {
+    setSelectValue(newValue as SingleValue<IconOption>)
+    onChange(newValue)
+  }
+
+  if (loading) return <div>Loading...</div>
+
+  return (
+    <Select
+      className='flex-1'
+      value={selectValue}
+      options={selectOptions}
+      components={{ Option: CustomOption, MenuList: MenuList }}
+      onChange={handleChange}
+      placeholder='搜索图标'
+      isSearchable={true}
+      isClearable={false}
+      styles={{
+        menuList: (base) => ({ ...base, padding: 0 })
+      }}
+    />
+  )
+}
+
+export default IconSelect
