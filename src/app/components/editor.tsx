191da25d1a70124913b232a862042c48b72c3f73
weizwz <1124725517@qq.com>
2025-05-06 17:31:17 +0800

    新增shadcnui组件

--- src/app/components/editor.tsx
+++ src/app/components/editor.tsx
@@ -1,11 +1,14 @@
 import { CoverProvider } from './coverContext'
+import EDitorSetting from './editorSetting'
 
 export default function Editor() {
   return (
     <div className="pt-14 h-full">
       <div className="h-full w-full flex overflow-hidden">
         <CoverProvider>
-          111
+          <div className='h-full md:w-1/4'>
+            <EDitorSetting />
+          </div>
         </CoverProvider>
       </div>
     </div>
---
+++ src/app/components/editorSetting.tsx
@@ -0,0 +1,7 @@
+export default function EDitorSetting() {
+  return (
+    <div className="h-full w-full overflow-y-auto">
+
+    </div>
+  )
+}
