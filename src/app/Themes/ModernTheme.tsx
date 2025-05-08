20e6f99452548908537a5e3b8b4c16d83fe1e49b
weizwz <1124725517@qq.com>
2025-05-08 16:55:55 +0800

    优化现代主题

--- src/app/Themes/ModernTheme.tsx
+++ src/app/Themes/ModernTheme.tsx
@@ -19,7 +19,7 @@ const ModernTheme: React.FC<ThemeProps> = ({ config }) => {
         <div className='h-full flex-1 max-h-[360px] max-w-[640px]'>
           <div className={`${font.value} bg-white px-12 justify-center text-left rounded-2xl h-full p-4 flex flex-col`}>
             <h1 className={`text-3xl md:text-5xl ${font?.lineHeight || 'leading-[1.2]'} font-bold text-gray-800`}>{title}</h1>
-            <h2 className='text-xl mt-10 font-semibold text-left '>{author}</h2>
+            <h2 className='text-2xl mt-10 font-semibold text-left '>{author}</h2>
           </div>
         </div>
       </div>
