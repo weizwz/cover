0b4fb508e024d3381b594e64a2cb065eda5cc97c
weizwz <1124725517@qq.com>
2025-05-09 11:06:03 +0800

    去除无用配置

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,34 +1,19 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+import type { Metadata } from 'next'
+import './globals.css'
 
 export const metadata: Metadata = {
-  title: "thisCover 封面生成器",
-  description: "免费、快速的封面生成器",
-};
+  title: 'thisCover 封面生成器',
+  description: '免费、快速的封面生成器'
+}
 
 export default function RootLayout({
-  children,
+  children
 }: Readonly<{
-  children: React.ReactNode;
+  children: React.ReactNode
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
+    <html lang='en'>
+      <body className={`antialiased`}>{children}</body>
     </html>
-  );
+  )
 }
