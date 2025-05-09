a77021f8fd97c280e22c097b0a01a12caa51a205
weizwz <1124725517@qq.com>
2025-05-09 19:52:49 +0800

    优化平铺主题

--- src/app/themes/outlineTheme.tsx
+++ src/app/themes/outlineTheme.tsx
@@ -7,7 +7,7 @@ const OutlineTheme: React.FC<ThemeProps> = ({ config }) => {
     <div className='w-full h-full bg-white'>
       <div className={`overflow-y-hidden text-gray-800 px-10 h-full relative`} style={{ backgroundColor: color.bgColor }}>
         <div className={`absolute top-0 left-0 w-full h-full z-1 ${pattern.value} filter-opacity-50`} />
-        <div className={`h-full rounded-2xl p-12 flex flex-col justify-center gap-12 relative z-10`}>
+        <div className={`h-full rounded-2xl p-12 flex flex-col justify-center relative z-10`}>
           {customIcon ? (
             <div className='m-6'>
               <img src={customIcon} alt='img' className='rounded-full object-cover w-24 h-24 bg-white p-1 border-white' />
@@ -17,7 +17,7 @@ const OutlineTheme: React.FC<ThemeProps> = ({ config }) => {
               <img className='w-18 h-18' src={`https://api.iconify.design/simple-icons/${icon.value}.svg?color=%23fff`} alt={`${icon.label} icon`} />
             </div>
           )}
-          <div className={`${font.value} mb-16`}>
+          <div className={`${font.value} mb-16 mt-8`}>
             <h1 className={`text-5xl ${font?.lineHeight || 'leading-[1.2]'} font-bold text-white text-shadow-lg text-shadow-black`}>{title}</h1>
             <h2 className='text-2xl mt-4 font-semibold text-white text-shadow-sm text-shadow-black'>{author}</h2>
           </div>
