a087693a24d1bbcfdae7663b307b9ec20ce5e616
weizwz <1124725517@qq.com>
2025-05-13 14:33:29 +0800

    优化图文对称主题

--- src/app/themes/stylishTheme.tsx
+++ src/app/themes/stylishTheme.tsx
@@ -7,49 +7,43 @@ import { CoverContext } from '../components/coverContext'
 import UnsplashSearch from '../components/unsplashSearch'
 
 const StylishTheme: React.FC<ThemeProps> = ({ config }) => {
-  const { title, color, pattern, author, icon, font, customIcon, size } = config
+  const { title, color, author, icon, font, customIcon } = config
   const { coverSetting, setCoverSetting } = useContext(CoverContext)
 
   return (
-    <div className={`h-full overflow-y-hidden flex flex-col`} style={{ backgroundColor: color.bgColor }}>
-      <div className='h-full flex flex-row items-center bg-white justify-center'>
-        <div className='h-full w-1/2 bg-white rounded-l-xl'>
-          <div className={`${font.value} p-12 justify-center text-left rounded-xl h-full flex flex-col`}>
-            <h1 className={`text-5xl ${font?.lineHeight || 'leading-[1.2]'} text-gray-800 font-bold text-shadow-lg`}>{title}</h1>
-            <div className='flex items-center mt-10 text-left'>
-              {customIcon ? (
-                <div className=' '>
-                  <img src={customIcon} alt='img' className='w-12 h-12 mr-2 rounded-full bg-white border border-white' />
-                </div>
-              ) : (
-                <div className='mr-4 items-center justify-center flex'>
-                  <img className='w-8 h-8' src={`https://api.iconify.design/simple-icons/${icon.value}.svg`} alt={`${icon.label} icon`} />
-                </div>
-              )}
-              <h2 className='text-2xl font-semibold text-left text-gray-800 text-shadow-sm'>{author}</h2>
-            </div>
+    <div className='w-full h-full overflow-y-hidden flex' style={{ backgroundColor: color.bgColor }}>
+      <div className={`w-1/2 h-full ${font.value} p-12 flex flex-col justify-center gap-6 bg-white text-gray-800`}>
+        <h1 className={`text-5xl ${font?.lineHeight || 'leading-[1.2]'} font-bold`}>{title}</h1>
+        <div className='flex items-center gap-4'>
+          <div className='items-center justify-center flex'>
+            {customIcon ? (
+              <img className='w-8 h-8' src={customIcon} alt='icon' />
+            ) : (
+              <img className='w-8 h-8' src={`https://api.iconify.design/simple-icons/${icon.value}.svg`} alt={`${icon.label} icon`} />
+            )}
           </div>
+          <h2 className='text-2xl font-semibold'>{author}</h2>
         </div>
-        <div className='w-1/2 h-full'>
-          {coverSetting.unsplashImage?.url ? (
-            <div className='relative w-full h-full flex group'>
-              <img src={coverSetting.unsplashImage.url} className='object-cover w-full h-full' alt='preview' />
-              <Button
-                className='hidden cursor-pointer absolute top-4 right-4 rounded-full text-center group-hover:flex'
-                variant='outline'
-                size='icon'
-                onClick={() =>
-                  setCoverSetting({ ...coverSetting, unsplashImage: { searchText: coverSetting.unsplashImage?.searchText || '', url: '', downloadLink: '' } })
-                }>
-                <X />
-              </Button>
-            </div>
-          ) : (
-            <div className='flex w-full h-full flex-col bg-white items-center justify-center'>
-              <UnsplashSearch largeImgPreview={false} />
-            </div>
-          )}
-        </div>
+      </div>
+      <div className='w-1/2 h-full'>
+        {coverSetting.unsplashImage?.url ? (
+          <div className='relative w-full h-full flex group'>
+            <img src={coverSetting.unsplashImage.url} className='object-cover w-full h-full' alt='preview' />
+            <Button
+              className='hidden cursor-pointer absolute top-4 right-4 rounded-full text-center group-hover:flex'
+              variant='outline'
+              size='icon'
+              onClick={() =>
+                setCoverSetting({ ...coverSetting, unsplashImage: { searchText: coverSetting.unsplashImage?.searchText || '', url: '', downloadLink: '' } })
+              }>
+              <X />
+            </Button>
+          </div>
+        ) : (
+          <div className='flex w-full h-full flex-col bg-white items-center justify-center'>
+            <UnsplashSearch largeImgPreview={false} />
+          </div>
+        )}
       </div>
     </div>
   )
