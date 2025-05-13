aa52b8acfa32e1f6f76e39cc69dbd7f9c6e83021
weizwz <1124725517@qq.com>
2025-05-13 11:58:30 +0800

    优化背景主题

--- src/app/themes/backgroundTheme.tsx
+++ src/app/themes/backgroundTheme.tsx
@@ -7,16 +7,14 @@ import { CoverContext } from '../components/coverContext'
 import UnsplashSearch from '../components/unsplashSearch'
 
 const BackgroundTheme: React.FC<ThemeProps> = ({ config }) => {
-  const { title, color, pattern, author, icon, font, customIcon, size } = config
+  const { title, color, author, icon, font, customIcon } = config
   const { coverSetting, setCoverSetting } = useContext(CoverContext)
 
   return (
     <div className='overflow-y-hidden flex flex-col w-full h-full' style={{ backgroundColor: color.bgColor }}>
       {coverSetting.unsplashImage?.url ? (
         <div className='w-full h-full relative flex group'>
-          <div className='h-full w-full'>
-            <img src={coverSetting.unsplashImage.url} className='object-cover h-full w-full' alt='preview' />
-          </div>
+          <img src={coverSetting.unsplashImage.url} className='object-cover h-full w-full' alt='preview' />
 
           <div className='h-full absolute top-0 right-0 left-0 p-12'>
             <Button
@@ -29,20 +27,18 @@ const BackgroundTheme: React.FC<ThemeProps> = ({ config }) => {
               <X />
             </Button>
 
-            <div className={`${font.value} text-left rounded-xl h-full flex flex-col justify-center`}>
-              {customIcon ? (
-                <div className=' '>
-                  <img src={customIcon} alt='img' className='w-12 h-12 m-2 rounded-full bg-white border-2 border-white' />
-                </div>
-              ) : (
-                <div className='flex items-center justify-center'>
+            <div className={`${font.value} h-full flex flex-col justify-center gap-6 pb-12 text-center text-white`}>
+              <div className='flex items-center justify-center'>
+                {customIcon ? (
+                  <img className='w-18 h-18' src={customIcon} alt='icon' />
+                ) : (
                   <img className='w-18 h-18' src={`https://api.iconify.design/simple-icons/${icon.value}.svg?color=%23fff`} alt={`${icon.label} icon`} />
-                </div>
-              )}
-              <h1 className={`mt-8 text-5xl ${font?.lineHeight || 'leading-[1.2]'} font-bold text-white text-center text-shadow-lg text-shadow-black`}>{title}</h1>
-              <div className='flex flex-col items-center mt-6 mb-16'>
-                <h2 className='text-2xl  font-semibold text-left text-white text-shadow-sm text-shadow-black'>{author}</h2>
+                )}
               </div>
+              <h1 className={`text-5xl ${font?.lineHeight || 'leading-[1.2]'} font-bold text-shadow-lg text-shadow-black`}>
+                {title}
+              </h1>
+              <h2 className='text-2xl font-semibold text-shadow-sm text-shadow-black'>{author}</h2>
             </div>
           </div>
         </div>
