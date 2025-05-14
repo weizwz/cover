85be82901d4b416eb8e527c33f6119a5928a93a2
weizwz <1124725517@qq.com>
2025-05-14 23:48:34 +0800

    修改尺寸命名

--- src/app/settings/sizes.ts
+++ src/app/settings/sizes.ts
@@ -4,35 +4,35 @@ export const SIZES: Size[] = [
     value: 'square vertical1-1 horizontal1-1'
   },
   {
-    label: '横板 2:1',
+    label: '横向 2:1',
     value: 'horizontal2-1'
   },
   {
-    label: '横板 16:9',
+    label: '横向 16:9',
     value: 'horizontal16-9'
   },
   {
-    label: '横板 4:3',
+    label: '横向 4:3',
     value: 'horizontal4-3'
   },
   {
-    label: '横板 3:2',
+    label: '横向 3:2',
     value: 'horizontal3-2'
   },
   {
-    label: '竖版 2:3',
+    label: '竖向 2:3',
     value: 'vertical2-3'
   },
   {
-    label: '竖版 3:4',
+    label: '竖向 3:4',
     value: 'vertical3-4'
   },
   {
-    label: '竖版 9:16',
+    label: '竖向 9:16',
     value: 'vertical9-16'
   },
   {
-    label: '竖版 1:2',
+    label: '竖向 1:2',
     value: 'vertical1-2'
   },
 ]
